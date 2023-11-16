2000ea3cb8adfe95fdea51970c86ec0cedd8e721
sharee-tech <mail.sharee@gmail.com>
2023-11-16 14:19:16 -0600

    first commit

---
+++ src/test/java/com/shareetech/springandreact/SpringAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareetech.springandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
